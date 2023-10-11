8dcd4e866697b3b696598f6283a8f7d38e022723
Tom Pollard <tpollard@mit.edu>
2023-10-11 16:44:44 -0400

    fix deprecated  function.

--- glowyr.py
+++ glowyr.py
@@ -40,7 +40,7 @@ def discrete_cmap(N, base_cmap=None):
     Create an N-bin discrete colormap from the input map
     By @jakevdp: https://gist.github.com/jakevdp/91077b0cae40f8f8244a
     """
-    base = plt.cm.get_cmap(base_cmap)
+    base = matplotlib.colormaps.get_cmap(base_cmap)
     color_list = base(np.linspace(0, 1, N))
     cmap_name = base.name + str(N)
     return base.from_list(cmap_name, color_list, N)
